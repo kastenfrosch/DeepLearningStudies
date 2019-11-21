3bcd1fc167ee829874ff45056dd565c14dc53c44
Johannes Morzeck <johannes.morzeck@gmail.com>
2019-11-21 23:16:51 +0100

    first model added

---
+++ image_classification/src/training.py
@@ -0,0 +1,116 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def create_dataset(verify):
+
+    # Download cifar10 dataset
+    (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+
+    # Normalize pixel values to be between 0 and 1
+    train_images, test_images = train_images / 255.0, test_images / 255.0
+
+    # Verify and show the imported data if needed
+    if verify:
+        verify_data(train_images, train_labels)
+
+    return train_images, train_labels, test_images, test_labels
+
+
+def verify_data(train_images, train_labels):
+
+    class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+                   'dog', 'frog', 'horse', 'ship', 'truck']
+
+    plt.figure(figsize=(10,10))
+    for i in range(25):
+        plt.subplot(5, 5, i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(train_images[i], cmap=plt.cm.binary)
+        plt.xlabel(class_names[train_labels[i][0]])
+    plt.show()
+
+
+def model_fn():
+
+    # Create CNN model
+    model = models.Sequential()
+
+    # Convolutional Layers
+    # First conv layer requires an input shape
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Dropout(0.3))
+
+    # Adding dense layers with softmax activation
+    model.add(layers.Flatten())
+    model.add(layers.Dense(128, activation='relu'))
+    model.add(layers.Dense(10, activation='softmax'))
+
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    # Output model summary
+    model.summary()
+
+    return model
+
+
+def start(epochs):
+
+    # Importing dataset
+    train_images, train_labels, test_images, test_labels = create_dataset(verify=True)
+
+    # Create model
+    model = model_fn()
+
+    # Training the model
+    history = model.fit(train_images, train_labels, epochs=epochs,
+                        validation_data=(test_images, test_labels))
+
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs_range = range(epochs)
+
+    plt.figure(figsize=(8, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+
+    plt.show()
+
+    test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+
+    print(test_acc)
+
+
+if __name__ == '__main__':
+    start(5)
+
+
