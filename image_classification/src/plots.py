fe07c0afd4caaf945ce585631764758b092766f9
Johannes Morzeck <johannes.morzeck@gmail.com>
2019-12-02 19:41:12 +0100

    added plots.py

---
+++ image_classification/src/plots.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+from config import config
+
+
+def plot_history(history):
+
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs_range = range(config.epochs)
+
+    plt.figure(figsize=(16, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+
+    plt.show()
+
